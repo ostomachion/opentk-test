21aad825b9293b6d17414801a1cb340abff522cc
josh@hufford.io <josh@hufford.io>
2024-05-21 23:44:41 -0400

    Clear color

--- OpenTKTest/Game.cs
+++ OpenTKTest/Game.cs
@@ -1,4 +1,6 @@
-﻿using OpenTK.Mathematics;
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 
 namespace OpenTKTest;
@@ -10,5 +12,31 @@ internal class Game(int width, int height)
     {
         this.CenterWindow(new Vector2i(width, height));
         base.OnLoad();
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs args)
+    {
+        base.OnUpdateFrame(args);
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs args)
+    {
+        GL.ClearColor(0, 0.5f, 0.5f, 1f);
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+        Context.SwapBuffers();
+        base.OnRenderFrame(args);
+    }
+
+    protected override void OnResize(ResizeEventArgs e)
+    {
+        GL.Viewport(0, 0, e.Width, e.Height);
+        width = e.Width;
+        height = e.Height;
+        base.OnResize(e);
+    }
+
+    protected override void OnUnload()
+    {
+        base.OnUnload();
     }
 }
